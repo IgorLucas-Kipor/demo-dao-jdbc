f1f049bf502ca8879ce96a4444981eaf5e731b5c
Igor Lucas <igorescritor@hotmail.com>
2021-05-12 13:39:44 -0300

    Implementando o método findById

--- Demo-Dao-JDBC/src/application/Program.java
+++ Demo-Dao-JDBC/src/application/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(32, "Lucas Mars", "lucas@gmail.com", new Date(), 3500.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
