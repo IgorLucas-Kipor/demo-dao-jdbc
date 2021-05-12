402da8618955f5b20e47e5956ca3566dff9a8e7c
Igor Lucas <igorescritor@hotmail.com>
2021-05-12 18:17:57 -0300

    Implementando Department findAll

--- Demo-Dao-JDBC/src/application/Program2.java
+++ Demo-Dao-JDBC/src/application/Program2.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
+		System.out.println("*** Test number one: Department findAll ***");
+		List<Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}
+		
 	}
 
 }
--- Demo-Dao-JDBC/src/model/dao/impl/DepartmentDaoJDBC.java
+++ Demo-Dao-JDBC/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,14 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -40,8 +46,32 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement(
+					"Select department.* from department");
+			rs = ps.executeQuery();
+			List<Department> list = new ArrayList<>();
+			while (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				list.add(dep);
+		}
+			return list;
+	} catch (SQLException e) {
+		throw new DbException(e.getMessage());
+	} finally {
+		DB.closeStatement(ps);
+		DB.closeResultSet(rs);
+	}
+	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+			Department dep = new Department();
+			dep.setId(rs.getInt("Id"));
+			dep.setName(rs.getString("Name"));
+			return dep;
+		}
 	}
 
-}
+
