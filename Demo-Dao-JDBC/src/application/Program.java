a9cb6e7d5efbf1755507af681e87cdef706585b7
Igor Lucas <igorescritor@hotmail.com>
2021-05-12 12:22:56 -0300

    Implementando a DaoFactory e SellerDaoJDBC

--- Demo-Dao-JDBC/src/application/Program.java
+++ Demo-Dao-JDBC/src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(32, "Lucas Mars", "lucas@gmail.com", new Date(), 3500.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
